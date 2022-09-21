79fa8a65e2ad4c304b277654da5cb3edfb41c07e
YAS-OKA <108788135+YAS-OKA@users.noreply.github.com>
2022-09-21 21:00:37 +0900

    名前入力ボードの導入
    nyuuryokuBoardファイルで作成したものをそのまま移植

---
+++ nameBoard.cpp
@@ -0,0 +1,83 @@
+﻿#include "stdafx.h"
+#include "nameBoard.h"
+
+nameBoard::nameBoard(Font f, Vec2 v, int32 Limit) :len(Limit), font(f), LeftTop(v)
+{
+	
+}
+
+bool nameBoard::isEnter()
+{
+	return Enter;
+}
+
+void nameBoard::Update()
+{
+	if (KeyUp.down())
+	{
+		nowmoji.colmn--;
+	}
+	if (KeyDown.down())
+	{
+		nowmoji.colmn++;
+	}
+	if (KeyLeft.down())
+	{
+		nowmoji.row--;
+	}
+	if (KeyRight.down())
+	{
+		nowmoji.row++;
+	}
+	nowmoji.row = (nowmoji.row + 13) % 13;
+	nowmoji.colmn = (nowmoji.colmn + 5) % 5;
+
+	if (KeyZ.down())
+	{
+		if (nowmoji.colmn == 4)
+		{
+			switch (nowmoji.row)
+			{
+			case 11:
+				if (not name.isEmpty())
+					name.pop_back();
+				break;
+			case 12:
+				Enter = true;
+				break;
+			default:
+				if (name.size() < len)
+					name += board[nowmoji.colmn][nowmoji.row];
+				break;
+			}
+		}
+		else
+		{
+			if (name.size() < len)
+				name += board[nowmoji.colmn][nowmoji.row];
+		}
+	}
+}
+
+void nameBoard::Draw()
+{
+	Vec2 p = LeftTop;
+	for (int32 i = 0; i < 5; i++)
+	{
+		for (int32 j = 0; j < 13; j++)
+		{
+			if (nowmoji.row == j && nowmoji.colmn == i)
+				font(board[i][j]).draw(p.x, p.y, ColorF(Palette::Lavenderblush));
+			else
+				font(board[i][j]).draw(p.x, p.y , ColorF(Palette::Black));
+			p.x += font.fontSize() * board[i][j].length()+3;
+		}
+		p.x = LeftTop.x;
+		p.y += font.fontSize()+3;
+	}
+}
+
+String nameBoard::GetName()
+{
+	return name;
+}
---
+++ nameBoard.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+class nameBoard
+{
+private:
+	String board[5][13] =
+	{
+		{U"A",U"B",U"C",U"D",U"E",U"F",U"G",U"H",U"I",U"J",U"K",U"L",U"M"},
+		{U"N",U"O",U"P",U"Q",U"R",U"S",U"T",U"U",U"V",U"W",U"X",U"Y",U"Z"},
+		{U"a",U"b",U"c",U"d",U"e",U"f",U"g",U"h",U"i",U"j",U"k",U"l",U"m"},
+		{U"n",U"o",U"p",U"q",U"r",U"s",U"t",U"u",U"v",U"w",U"x",U"y",U"z"},
+		{U"<",U">",U"!",U"?",U";",U"/",U"[",U"]",U"^",U".",U"_",U"戻る",U"決定"},
+	};
+
+	struct nowSelecting {
+		int32 row = 0;
+		int32 colmn = 0;
+	}nowmoji;
+
+	Vec2 LeftTop;//左上の座標
+	String name;
+	bool Enter = false;
+	Font font;
+	int32 len;
+public:
+	nameBoard(Font f,Vec2 v,int32 Limitlen = 3);//文字数制限
+	bool isEnter();
+	void Update();
+	void Draw();
+	String GetName();
+};
+
